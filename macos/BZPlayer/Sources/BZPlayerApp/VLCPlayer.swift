83bfb73c68f4e3a64331f5ad31ad85f3378dabef
Lin Yi <dbghpixian@outlook.com>
2026-06-10 02:10:03 +0800

    修复：禁用 VideoToolbox 硬件解码以彻底解决快退时画面卡死 5s 的问题

--- macos/BZPlayer/Sources/BZPlayerApp/VLCPlayer.swift
+++ macos/BZPlayer/Sources/BZPlayerApp/VLCPlayer.swift
@@ -22,6 +22,7 @@ final class VLCPlayer: NSObject {
     override init() {
         let library = VLCLibrary(options: [
             "--avcodec-hw=none",
+            "--no-videotoolbox",
             "--freetype-font=/System/Library/Fonts/STHeiti Light.ttc",
             "--subsdec-encoding=GB18030"
         ])
@@ -47,6 +48,8 @@ final class VLCPlayer: NSObject {
         let media = VLCMedia(url: url)
         media.addOption(":subsdec-encoding=GB18030")
         media.addOption(":freetype-font=/System/Library/Fonts/STHeiti Light.ttc")
+        media.addOption(":avcodec-hw=none")
+        media.addOption(":no-videotoolbox")
         currentMedia = media
         mediaPlayer.media = media
     }
